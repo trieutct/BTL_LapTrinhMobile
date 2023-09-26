fbe7f0217e6f9e7087fff0f6d91c79023cc915d1
trieutct <96654274+trieutct@users.noreply.github.com>
2023-09-26 20:42:11 +0700

    push giao dien

--- app/src/main/java/com/example/app_thibanglaixe/MainActivity.java
+++ app/src/main/java/com/example/app_thibanglaixe/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.app_thibanglaixe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    CountDownTimer countDownTimer;
+    ProgressBar progressBar;
+    int timerValue=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar=findViewById(R.id.progressbar);
+        countDownTimer=new CountDownTimer(5000,85) {
+            @Override
+            public void onTick(long l) {
+                timerValue=timerValue+2;
+                progressBar.setProgress(timerValue);
+            }
+            @Override
+            public void onFinish() {
+                Intent intent=new Intent(MainActivity.this,MainTrangChuActivity.class);
+                startActivity(intent);
+            }
+        }.start();
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent=new Intent(MainActivity.this,MainTrangChuActivity.class);
+//                startActivity(intent);
+//            }
+//        },5000);
     }
 }
---
+++ app/src/main/java/com/example/app_thibanglaixe/MainTrangChuActivity.java
@@ -0,0 +1,25 @@
+package com.example.app_thibanglaixe;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainTrangChuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_trang_chu);
+
+
+
+    }
+}
